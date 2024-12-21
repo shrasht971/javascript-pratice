6c8cb0fc65c2e414f31b94f2ba6f2916715de4ec
shrasht971 <94127570+shrasht971@users.noreply.github.com>
2024-12-21 23:18:27 +0530

    Nested condition problem.

---
+++ Condition/IfElse.js
@@ -0,0 +1,27 @@
+// let num=20
+// if(num>=18){
+//     console.log('I can vote')
+// }
+// else{
+//     console.log(`I can't vote `)
+// }
+
+let number = 2;
+let age = 20;
+if(number==1){
+    console.log('A');
+}
+else if(number == 2){
+    if(age>=18){
+        console.log('You can gave Vote.')
+    }
+    console.log('B');
+}
+else if(number==3){
+    console.log('C');
+}
+else {
+    console.log('F');
+}
+
+
