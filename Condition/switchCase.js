c93cfa1592b6ae3e70d7c013ef5eb7a627f6f35e
shrasht971 <94127570+shrasht971@users.noreply.github.com>
2024-12-21 23:32:10 +0530

    Use switch case condition in week day find problem.

---
+++ Condition/switchCase.js
@@ -0,0 +1,19 @@
+let day = 'thursday';
+
+switch(day){
+    case 'sunday': console.log('Sunady');
+    break;
+    case 'monday': console.log('Monday');
+    break;
+    case 'tuesday': console.log('Tuesday');
+    break;
+    case 'wednesday': console.log('Wednesday');
+    break;
+    case 'thursday': console.log('Thursday');
+    break;
+    case 'friday': console.log('Friday');
+    break;
+    case 'saturday': console.log('Saturday');
+    break;
+    
+}
