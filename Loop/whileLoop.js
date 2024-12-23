f18100f2d2ea5402deb682de269c4c42421379ee
shrasht971 <94127570+shrasht971@users.noreply.github.com>
2024-12-24 00:46:50 +0530

    use while loop with break and continue keyword and write the code print 2 table.

---
+++ Loop/whileLoop.js
@@ -0,0 +1,32 @@
+// let i=1;
+
+// while(i<=10){
+//     console.log(2*i);
+//     i++;
+// }
+
+
+
+// let i=1;
+// while(i<=10){
+//     if(i==5)
+//         break;
+//     else{
+//         console.log(2*i);
+//         i++;
+//     }
+// }
+
+let i=1;
+while(i<5){
+
+    
+    if(i==4){
+        continue;
+        i++;
+    }
+    else
+        console.log(i);
+        i++;
+    
+}
