6e548a86e26e17725b7b6a9a2af17fed0a219aba
shrasht971 <94127570+shrasht971@users.noreply.github.com>
2024-12-23 14:14:50 +0530

    for loop with use break and continue keywoard.

---
+++ Loop/forLoop.js
@@ -0,0 +1,17 @@
+for(let i=1;i<=5;i++)
+{
+    if(i==4)
+        break;
+    else{
+        console.log(i);
+    }
+
+}
+
+for(let i=1;i<=5;i++){
+    if(i==4)
+        continue;
+    else{
+        console.log(i);
+    }
+}
